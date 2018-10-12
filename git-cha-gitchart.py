feeaab1143edecb16c0c72dde139a34141dfdd82
scarletferrante <scarlet.ferrante@protonmail.com>
2018-10-11 23:54:18 -0400

    Commented outline

--- git-cha-gitchart.py
+++ git-cha-gitchart.py
@@ -1,6 +1,16 @@
 
 def run():
+	# Get the author name from .gitconfig and print
+	# Find gitconfig file
+	# Open file
+	# Read name entry
+	# Output it to the screen
+	# If the author of the commit is you, plot it
+	# Get the time stape of the commit
+	# Plot a point at that time
+	# Display
 	print("Hello World!")
 
 if __name__ == '__main__':
 	run()
+
