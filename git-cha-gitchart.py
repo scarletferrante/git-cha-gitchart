5849f10adf469ac949a15e0381a710f886f68a4d
scarletferrante <scarlet.ferrante@protonmail.com>
2018-10-17 10:28:49 -0400

    Got user's name

--- git-cha-gitchart.py
+++ git-cha-gitchart.py
@@ -1,28 +1,28 @@
 import os
+from os.path import expanduser
 
 def run():
 	# Get the author name from .gitconfig and print
-	# Find gitconfig file
-	# ~/.gitconfig
-
-	# Open file
-	path = input('Location of .gitconfig file: ')
+	# Find gitconfig file ~/.gitconfig
+	path = expanduser("~")
 	print(path)
 	os.chdir(path)
-
+	# Open file
 	file = open('.gitconfig', 'r')
 	contents = file.read()
 	file.close()
-	print(contents)
-
 	# Read name entry
-	
+	contents_list = contents.split()
+	#print("contents_list = ", contents_list)
+	for word in contents_list:
+		if word == "name":
+			name = contents_list[contents_list.index(word) + 2]
 	# Output it to the screen
+	print("name = ", name)
 	# If the author of the commit is you, plot it
 	# Get the time stape of the commit
 	# Plot a point at that time
 	# Display
-	print("Hello World!")
 
 if __name__ == '__main__':
 	run()
